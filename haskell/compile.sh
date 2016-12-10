cf43fa303e3bb48a6f8f805affbaa4d38c960b42
Pau Argelaguet <pau@outlook.com>
2016-12-10 23:51:27 +0100

    Updating scripts

--- haskell/compile.sh
+++ haskell/compile.sh
@@ -4,3 +4,7 @@ mkdir -p bin/
 antlr -gt src/lang.g -o bin/
 dlg -ci bin/parser.dlg bin/scan.c -o bin/
 g++ -o bin/lang bin/lang.c bin/scan.c bin/err.c -I /usr/include/pccts/ -w -std=c++11
+rm bin/err.c bin/lang.c bin/mode.h bin/parser.dlg bin/scan.c bin/tokens.h
+
+ghc src/interpreter.hs -o bin/interpreter
+rm src/interpreter.o src/interpreter.hi
--- haskell/test.sh
+++ haskell/test.sh
@@ -2,19 +2,3 @@
 
 ./clean.sh
 ./compile.sh
-
-bin/lego < testcases/complex.inp > testcases/complex.tmp
-diff testcases/complex.out testcases/complex.tmp
-rm testcases/complex.tmp
-
-bin/lego < testcases/errors.inp > testcases/errors.tmp
-diff testcases/errors.out testcases/errors.tmp
-rm testcases/errors.tmp
-
-bin/lego < testcases/pops.inp > testcases/pops.tmp
-diff testcases/pops.out testcases/pops.tmp
-rm testcases/pops.tmp
-
-bin/lego < testcases/sample.inp > testcases/sample.tmp
-diff testcases/sample.out testcases/sample.tmp
-rm testcases/sample.tmp
