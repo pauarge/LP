a0869bbfdfb17389f0f79368bdcf647b968a0dce
Pau Argelaguet <pau@outlook.com>
2016-12-23 02:38:22 +0100

    Getting data from the server

--- scripting/cerca.py
+++ scripting/cerca.py
@@ -5,28 +5,32 @@ import argparse
 import sys
 
 URL_BICING = 'http://wservice.viabicing.cat/v1/getstations.php?v=1'
-URL_EVENTS = 'http://www.bcn.cat/tercerlloc/agenda cultural.xml'
+URL_EVENTS = 'http://www.bcn.cat/tercerlloc/agenda_cultural.xml'
 URL_PARKING = 'http://www.bcn.cat/tercerlloc/Aparcaments.xml'
 
 
 def fetch_stations():
-    pass
+    data = urlopen(URL_BICING).read()
+    root = ET.fromstring(data)
+    return root.iter('station')
 
 
 def fetch_events():
-    pass
+    data = urlopen(URL_EVENTS).read()
+    root = ET.fromstring(data)
+    return root.find('search').find('queryresponse').find('list').find('list_items').iter('row')
 
 
 def fetch_parking():
-    pass
+    data = urlopen(URL_PARKING).read()
+    root = ET.fromstring(data)
+    return root.find('search').find('queryresponse').find('list').find('list_items').iter('row')
 
 
 def fetch_data():
-    data = urlopen(URL_BICING).read()
-
-    root = ET.fromstring(data)
-    for child in root.iter('station'):
-        print(child.tag)
+    fetch_stations()
+    fetch_events()
+    fetch_parking()
 
 
 def main(argv):
